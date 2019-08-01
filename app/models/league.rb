6c9bde126374708ba3fbed6d3f5d628cb787d531
github username <jpsypek@gmail.com>
2019-08-01 11:05:03 -0600

    add seeds

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,5 @@
 class League < ApplicationRecord
   belongs_to :organization
+  has_many :teams
   accepts_nested_attributes_for :teams, allow_destroy: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Player.destroy_all
+Team.destroy_all
+TeamPlayer.destroy_all
+League.destroy_all
+Organization.destroy_all
+
+mile_high = Organization.create(name: "Play Mile High")
+tues_soccer = League.create(sport: "Soccer", sex: "Coed", organization: mile_high, day: "Tuesday", season: "Summer" )
+jack = Player.create(first_name: "Jack", last_name: "Sypek")
+slayers = Team.create(name: "Dragon Slayers", league: tues_soccer)
+TeamPlayer.create(player: jack, team: slayers)
